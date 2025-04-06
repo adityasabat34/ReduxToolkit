39c49905b09e3751af7f8e72b4daa658027a8a58
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-04-06 23:53:44 +0530

    store and first Slice reducer created

---
+++ reduxTK/src/feature/todo/todoSlice.js
@@ -0,0 +1,16 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = { id: nanoid(), text: action.payload };
+      state.todos.push(todo);
+    },
+  },
+});
